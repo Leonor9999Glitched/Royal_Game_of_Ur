bed748d08b9bb21a4c870d430466c957325bb4d4
Leonor <leonorpatriciasf@hotmail.com>
2021-05-02 18:57:08 +0100

    Options

--- Royal_Game_of_Ur/Program.cs
+++ Royal_Game_of_Ur/Program.cs
@@ -6,7 +6,33 @@ namespace Royal_Dame_of_Ur
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string escolha;
+            
+            Console.WriteLine("Bem-vindos ao Royal Game of Ur!");
+            Console.WriteLine("São precisos dois jogadores para este jogo.");
+            Console.WriteLine("Mas primeiro, é preciso ver o que o jogador deseja fazer.");
+            Console.WriteLine("1 - Jogar o jogo");
+            Console.WriteLine("2 - Regras do jogo");
+            Console.WriteLine("3 - Sair do programa");
+
+            escolha = Console.ReadLine();
+
+
+            do
+            {
+                if (escolha == "1")
+                {
+                    Console.WriteLine("Vamos jogar!!!");
+                    escolha = Console.ReadLine();
+                }
+                else if (escolha == "2")
+                {
+                    Console.WriteLine("Regras em breve!!!");
+                    escolha = Console.ReadLine();
+                }
+            } while (escolha != "3");
+
+            Console.WriteLine("Vejo-te em breve!!!");
         }
     }
 }
