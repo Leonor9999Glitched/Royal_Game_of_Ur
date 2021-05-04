dbecd0196e456c6280131cf4544e1c61cefe5afd
Leonor <leonorpatriciasf@hotmail.com>
2021-05-04 15:50:28 +0100

    Criando a Board

--- Royal_Game_of_Ur/Board.cs
+++ Royal_Game_of_Ur/Board.cs
@@ -4,6 +4,9 @@ namespace Royal_Game_of_Ur
 {
     public class Board
     {
-        
+        public bool Winner()
+        {
+            return true;
+        }
     }
 }
--- Royal_Game_of_Ur/Game.cs
+++ Royal_Game_of_Ur/Game.cs
@@ -4,9 +4,20 @@ namespace Royal_Game_of_Ur
 {
     public class Game
     {
+        private Board board;
+
+        public Game()
+        {
+            board = new Board();
+        }
+
         public void Play()
         {
             Console.WriteLine("Game very soon!!!");
+            while (!board.Winner())
+            {
+                Console.WriteLine("Game very soon!!!");
+            }
         }
     }
 }
