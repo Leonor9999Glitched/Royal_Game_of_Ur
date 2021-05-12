d9ad9c1baefab782f8ad8016131354171e3eaa0c
André Cosme <andrecosme77@gmail.com>
2021-05-12 23:04:46 +0100

    Criar class Bordblock
    com alguns comentarios

---
+++ Royal_Game_of_Ur/BoardBlock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Royal_Dame_of_Ur
+{
+    /// <summary>
+    /// A BoardBlock class é responsavel por guardar dados,
+    /// cordena instancias, posições no board,
+    /// se está ocupado por outra peça, por qual jogador.
+    /// </summary>
+    class BoardBlock
+    {
+    }
+}
+
+
+
+
