52ade4e084fe2e38b6c59caa67c6ee8450f7614d
André Cosme <andrecosme77@gmail.com>
2021-05-12 23:15:52 +0100

    class BoardBlock
    acrescentar alguns atributos e os seus respectivos comentarios

--- Royal_Game_of_Ur/BoardBlock.cs
+++ Royal_Game_of_Ur/BoardBlock.cs
@@ -4,13 +4,38 @@ using System.Collections.Generic;
 
 namespace Royal_Dame_of_Ur
 {
-    /// <summary>
-    /// A BoardBlock class é responsavel por guardar dados,
-    /// cordena instancias, posições no board,
-    /// se está ocupado por outra peça, por qual jogador.
-    /// </summary>
+    
+    // A BoardBlock class é responsavel por guardar dados,
+    // cordena instancias, posições no board,
+    // se está ocupado por outra peça, por qual jogador.
+    
     class BoardBlock
     {
+        string BoardBlockName;
+
+        public enum OwnerTypes { P1, P2, BOTH }
+        
+        // verifica qual player esta apto para ocupar este block
+        
+        private PlayerType owner;
+        
+        // Onde este Block está localizado no board
+        
+        private int position;
+        
+        // Se este Block está atualmente ocupado por uma Piece
+        
+        private bool shared;
+        
+        // Se este Block contêm uma rosette,
+        // o jogador que calhar neste Block
+        // está salvo e recebe um novo turno.
+        
+        private bool isRosette;
+        
+        // OccupationData dos Block´s
+        
+        private OccupationData occupationData = new OccupationData();
     }
 }
 
