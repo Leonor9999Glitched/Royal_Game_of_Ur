eeb8eefb4714ae31a16e3cddbefc08cea9fa64f0
André Cosme <andrecosme77@gmail.com>
2021-05-13 14:43:33 +0100

    BoardBlock e Block
    Criar e alterar codigo

---
+++ Royal_Game_of_Ur/Block.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Royal_Dame_of_Ur
+{
+    public class Block
+    {
+        public enum BlockType
+        {
+            Normal,
+            Rosette,
+            Start
+        }
+        public enum TerritoryType
+        {
+            Neutral,
+            Player1,
+            Player2
+        }
+
+        private BlockType blockType = BlockType.Normal;
+
+        private TerritoryType territoryType = TerritoryType.Neutral;
+
+        public BlockType GetBlockType()
+        {
+            return blockType;
+        }
+
+        public TerritoryType GetTerritoryType()
+        {
+            return territoryType;
+        }
+    }
+}
--- Royal_Game_of_Ur/BoardBlock.cs
+++ Royal_Game_of_Ur/BoardBlock.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-namespace Royal_Dame_of_Ur
+namespace Royal_Game_of_Ur
 {
     
     // A BoardBlock class é responsavel por guardar dados,
@@ -36,20 +36,125 @@ namespace Royal_Dame_of_Ur
         // OccupationData dos Block´s
         
         private OccupationData occupationData = new OccupationData();
-        
-        
+
+
         //Construtores
 
-        
-        // Principalmente usado para criar os block´s usados pelo player
-        public BoardBlock(PlayerType owner, int position)
+
+        public BoardBlock(int pos)
         {
-            BoardBlockName = owner.ToString() + " BoardBlock " + position;
-            this.owner = owner;
-            this.position = position;
+            position = pos;
             isRosette = (position == 3 || position == 7 || position == 13);
             shared = owner != PlayerType.Neutral;
         }
+        public BoardBlock()
+        {
+            position = -1;
+            isRosette = (position == 3 || position == 7 || position == 13);
+        }
+
+        public BoardBlock(bool isFinalDestination, PlayerType owner)
+        {
+            BoardBlockName = "Final Destination BoardBlock";
+            this.owner = owner;
+            shared = false;
+            Console.WriteLine("Meta Final");
+        }
+
+        // zona onde estao GET/SET´s
+
+        // Para Aceder
+        public int GetPosition()
+        {
+            return position;
+        }
+        public bool IsShared()
+        {
+            return shared;
+        }
+
+        // Para alterar
+        public void SetPosition(int pos)
+        {
+            position = pos;
+        }
+
+        public bool IsRosette
+        {
+            get
+            {
+                return isRosette;
+            }
+
+            set
+            {
+                isRosette = value;
+            }
+        }
+
+        //Operações
+
+        public OccupationData GetOccupationData(PlayerType owner, int pieceid)
+        {
+            if (occupationData.IsOccupied)
+            {
+                // se o jogador esta a ultrapassar uma peça sua
+                if (occupationData.PlayerId == (int)owner)
+                    occupationData.SelfOccupied = true;
+                // se o jogador esta a ultrapassar uma peça do seu oponente
+                else
+                {
+                    occupationData.OverlapperId = pieceid;
+                    occupationData.OpponentOverlap = true;
+                }
+
+                occupationData.OverlapLocation = position;
+            }
+
+            return occupationData;
+        }
+
+
+
+        public OccupationData Occupy(int playerid, int pieceid, out bool success)
+        {
+            // Se este Block não esta ocupado
+            if (!occupationData.IsOccupied)
+            {
+                occupationData.Occupy(playerid, pieceid);
+                success = true;
+            }
+            // Se este Block esta ocupado
+            else
+            {
+                success = false;
+
+                // se o jogador esta a ultrapassar uma peça sua
+                if (occupationData.PlayerId == playerid)
+                    occupationData.SelfOccupied = true;
+                // se o jogador esta a ultrapassar uma peça do seu oponente
+                else
+                {
+                    occupationData.OverlapperId = pieceid;
+                    occupationData.OpponentOverlap = true;
+                }
+
+                occupationData.OverlapLocation = position;
+            }
+
+            return occupationData;
+        }
+
+        public void Occupy(int playerid, int pieceid)
+        {
+            occupationData.Occupy(playerid, pieceid);
+        }
+
+        public void Unoccupy()
+        {
+            occupationData.Unoccupy();
+        }
+
     }
 }
 
