c7c65e1a58fb26d8cba0b574f76db8769b5fa915
Leonor <leonorpatriciasf@hotmail.com>
2021-05-13 21:23:28 +0100

    New Board

--- Royal_Game_of_Ur/Board_V2.cs
+++ Royal_Game_of_Ur/Board_V2.cs
@@ -6,9 +6,13 @@ namespace Royal_Game_of_Ur
 {
     public class Board_V2
     {
+        private char [][] board = new char [3][];
+        
         public Board_V2()
         {
-            
+            board[0] = new char[8] {'#', 'O', 'O', 'I', ' ', ' ', 'F', 'O'};
+            board[1] = new char[8] {'O', 'O', 'O', '#', 'O', 'O', 'O', 'O'};
+            board[2] = new char[8] {'#', 'O', 'O', 'I', ' ', ' ', 'F', 'O'};
         } 
     }
 }
