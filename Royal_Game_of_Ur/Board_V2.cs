ef1ab48d1643f9193e78d14ce8c641e6039a8cf5
Leonor <leonorpatriciasf@hotmail.com>
2021-05-13 13:58:53 +0100

    New Piece

---
+++ Royal_Game_of_Ur/Board_V2.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Royal_Game_of_Ur
+{
+    public class Board_V2
+    {
+        public Piece 
+    }
+}
---
+++ Royal_Game_of_Ur/Piece.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+
+namespace Royal_Game_of_Ur
+{
+    public class Piece
+    {
+        private int piece_ID = -1;
+        private PlayerType owner = PlayerType.Player1;
+        private bool initialized = false;
+        private bool scored;
+
+        //private movement;
+
+        //Inicializar a peça do jogo
+
+
+    }
+}
