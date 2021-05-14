db63d59a5c5f1f2cb914b65392b27421eaed4502
André Cosme <andrecosme77@gmail.com>
2021-05-14 11:31:46 +0100

    Criar class Player.cs

---
+++ Royal_Game_of_Ur/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Royal_Game_of_Ur
+{
+    public class Player
+    {
+        public string Name { get; }
+        public int Score { get; }
+
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Score})";
+        }
+    }
+}
