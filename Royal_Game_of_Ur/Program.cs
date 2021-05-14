61b53d0a687735fecbf9c2b97293959399cce7de
André Cosme <andrecosme77@gmail.com>
2021-05-14 11:38:36 +0100

    Fix

--- Royal_Game_of_Ur/Program.cs
+++ Royal_Game_of_Ur/Program.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
-namespace Royal_Dame_of_Ur
+namespace Royal_Game_of_Ur
 {
-    class Programcs
+    public class Program
     {
+        private static void Main()
+        {
+            // List of player is our model
+            List<Player> list = new List<Player>()
+            { new Player("Player1", 100), new Player("Player2", 100) };
+
+            // Create controllers
+            Controller controller = new Controller(list);
+
+            // Create view
+            IView view = new GameUI(controller); //, list);
+
+            // Start program
+            controller.Run(view);
+        }
     }
 }
