f2d4df61cf0876a47972a2e440799532dc921fb4
André Cosme <andrecosme77@gmail.com>
2021-05-14 11:29:45 +0100

    Criar um IView e um Controller

---
+++ Royal_Game_of_Ur/IView.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Royal_Game_of_Ur
+{
+    public interface IView
+    {
+        int MainMenu();
+
+        void InvalidOption();
+
+        void ShowPlayers(IEnumerable<Player> players);
+
+        Player AskForPlayer();
+
+        int AskForMinimumScore();
+    }
+}
